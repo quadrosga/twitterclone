077c8594f84e7ca8d74ac432cb4fe6858f38aa2b
Gabriela Quadros_do_Amaral <quadros.ga@hotmail.com>
2025-03-18 21:21:55 -0300

    update db information

--- twitterclone/settings.py
+++ twitterclone/settings.py
@@ -119,18 +119,7 @@ WSGI_APPLICATION = "twitterclone.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'twitterclone',  # Database name from PythonAnywhere
-        'USER': 'quadrosga',       # Username from PythonAnywhere
-        'PASSWORD': 'twitterclone',   # Password from PythonAnywhere
-        'HOST': 'quadrosga.mysql.pythonanywhere-services.com',  # Host from PythonAnywhere
-        'PORT': '3306',  # Default MySQL port
-    }
-}
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'twitterclone',  # Database name from PythonAnywhere
+        'NAME': 'quadrosga$twitterclone',  # Database name from PythonAnywhere
         'USER': 'quadrosga',       # Username from PythonAnywhere
         'PASSWORD': 'twitterclone',   # Password from PythonAnywhere
         'HOST': 'quadrosga.mysql.pythonanywhere-services.com',  # Host from PythonAnywhere
